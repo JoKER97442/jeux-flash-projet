19de3a1fb9eae374c5b1e229f10270ad5f781f63
JoKER97442 <jonathan.kerbidi96@gmail.com>
2021-08-02 16:01:32 +0400

    vue

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import FooterTemplate from './FooterTemplate.js';
+import HeaderTemplate from './HeaderTemplate.js';
+import MainTemplate from './MainTemplate.js';
+
+new Vue({
+    el: '#app',
+    //Ajouter le nom du composant.
+    components: { FooterTemplate, HeaderTemplate, MainTemplate },
+    data: {},
+});
